e8129aa0066b23edadfdff51284a9d31355568ff
Luis Gutierrez <luis.gutierrez@koombea.com>
2014-10-18 22:51:46 -0500

    fixed popular movies - added movie details

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+
   root to: 'home#index'
+
+  resources :users
+
+  get 'movie_details/:movie_id' => 'home#movie_details', as: :movie_details
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
